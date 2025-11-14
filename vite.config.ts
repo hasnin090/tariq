1ce606b41fbfcf2ae3c79b89b97aaf635d47c63a
hasnin090 <fcblon@yahoo.com>
2025-11-14 23:30:23 +0300

    fix: explicitly set postcss config path in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.cjs'
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
     cssCodeSplit: true
-  },
-  css: {
-    postcss: './postcss.config.cjs'
   },
   server: {
     port: 3000,
